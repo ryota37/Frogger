966300b719fdaace6cd378ce062de2a652c1d1cb
ryota37 <ryota.nakahama@access-company.com>
2025-05-31 16:22:05 +0900

    Removed unnecessary codes

--- Frogger/Main.cpp
+++ Frogger/Main.cpp
@@ -20,24 +20,6 @@ void DrawCircle(const Circle& circle)
 	circle.draw(Palette::Aqua);
 }
 
-int random_number(int n)
-{
-	std::srand(std::time(0));
-	int random_number = std::rand() % n;
-	return random_number;
-}
-
-std::vector<int> random_pair_generator()
-{
-	int n, m;
-	do
-	{
-		n = random_number(7);
-		m = random_number(6);
-	} while (!((n % 2 == 0) ^ (m % 2 == 0)));
-	return std::vector<int> {n, m};
-}
-
 enum class Direction { Up, Down, Left, Right };
 
 namespace Coordinate
@@ -46,10 +28,8 @@ namespace Coordinate
 	constexpr int TOP_GRID_Y = 50;
 	constexpr int BOTTUM_GRID_X = 750;
 	constexpr int BOTTUM_GRID_Y = 550;
-
-	std::vector<int> pair = random_pair_generator();
-	int GOAL_X = 50 + 100 * pair[0];
-	int GOAL_Y = 50 + 100 * pair[1];
+	constexpr int START_GRID_X = 350;
+	constexpr int START_GRID_Y = 550;
 }
 
 bool isWallUp(Circle& circle, Grid<int32>& grid, int grid_x, int grid_y)
@@ -103,32 +83,6 @@ void UpdateCircle(Circle& circle, Grid<int32>& grid)
 	if (KeyRight.down() && !isWall(circle, grid, Direction::Right)) circle.x += 100;
 }
 
-bool isAllGridPassed(const Grid<int32>& grid)
-{
-	for (int32 y = 0; y < grid.height(); ++y)
-	{
-		for (int32 x = 0; x < grid.width(); ++x)
-		{
-			if (grid[y][x] == 0) return false;
-		}
-	}
-	return true;
-}
-
-void PrintResult(const Circle& circle, const Grid<int32>& grid, const Font& font)
-{
-	if (circle.x != Coordinate::GOAL_X) return;
-	if (circle.y != Coordinate::GOAL_Y) return;
-	if (isAllGridPassed(grid)) { font(U"Success!!!").drawAt(Vec2{ 400, 300 }, Palette::Black); }
-	else { font(U"Failure!!!").drawAt(Vec2{ 400, 300 }, Palette::Black); }
-}
-
-void DrawGoalGrid()
-{
-	const RectF rect{ Coordinate::GOAL_X - 50, Coordinate::GOAL_Y - 50, 100 };
-	rect.draw(Palette::Olive);
-}
-
 bool isDirectionKeyPressed()
 {
 	if (KeyLeft.down()) return true;
@@ -138,32 +92,13 @@ bool isDirectionKeyPressed()
 	return false;
 }
 
-void VisualizeCircleRoute(const Circle& circle, Grid<int32>& grid)
-{
-	int grid_x = static_cast<int>(circle.x) / 100;
-	int grid_y = static_cast<int>(circle.y) / 100;
-
-	for (int32 y = 0; y < grid.height(); ++y)
-	{
-		for (int32 x = 0; x < grid.width(); ++x)
-		{
-			const RectF rect{ (x * 100), (y * 100), 100 };
-			if (x == grid_x && y == grid_y && grid[y][x] == 0)
-			{
-				// 円が格子を通った時に要素を0→1にする
-				++grid[y][x] += 1;
-			}
-		}
-	}
-}
-
 void Main()
 {
 	Scene::SetBackground(ColorF{ 0.8, 0.9, 1.0 });
 
 	// 8x6 の二次元配列を作成し、全ての要素を 0 で初期化する
 	Grid<int32> grid(8, 6);
-	Circle circle{ Coordinate::TOP_GRID_X,Coordinate::TOP_GRID_Y,25 };
+	Circle circle{ Coordinate::START_GRID_X,Coordinate::START_GRID_Y,25 };
 	const Font font{ FontMethod::MSDF, 48, Typeface::Bold };
 
 	while (System::Update())
@@ -173,11 +108,5 @@ void Main()
 		UpdateCircle(circle, grid);
 
 		DrawCircle(circle);
-
-		DrawGoalGrid();
-
-		VisualizeCircleRoute(circle, grid);
-
-		PrintResult(circle, grid, font);
 	}
 }
